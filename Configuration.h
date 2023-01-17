bcbc1fd5af7230411d012e6138fa4c1a821de904
Pirinvk <pirindroid@gmail.com>
2023-01-17 15:47:26 +0100

    decrecer velocidad

--- Configuration.h
+++ Configuration.h
@@ -1191,7 +1191,7 @@
  * Override with M201
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 1000, 1000, 10, 10000 }
+#define DEFAULT_MAX_ACCELERATION      { 1000, 1000, 8, 10000 }
 
 //#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
@@ -1206,9 +1206,9 @@
  *   M204 R    Retract Acceleration
  *   M204 T    Travel Acceleration
  */
-#define DEFAULT_ACCELERATION           500    // X, Y, Z and E acceleration for printing moves
-#define DEFAULT_RETRACT_ACCELERATION   800    // E acceleration for retracts
-#define DEFAULT_TRAVEL_ACCELERATION    900    // X, Y, Z acceleration for travel (non printing) moves
+#define DEFAULT_ACCELERATION           400    // X, Y, Z and E acceleration for printing moves
+#define DEFAULT_RETRACT_ACCELERATION   600    // E acceleration for retracts
+#define DEFAULT_TRAVEL_ACCELERATION    600    // X, Y, Z acceleration for travel (non printing) moves
 
 /**
  * Default Jerk limits (mm/s)
